84f1d65012f65668f45265a18fb6e5fcb4affc2e
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-10-08 16:07:38 +0300

    adding  4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function getListStudents(ArrayOfObjects, city, newGrades) {
+  let listOfStudents = [];
+  if (Array.isArray(ArrayOfObjects) === false) {
+    return listOfStudents;
+  }
+  listOfStudents = ArrayOfObjects.filter((item) => item.location === city);
+  listOfStudents.map((item) => item.grade = 'N/A');
+  listOfStudents.map((item) => {
+    for (const student of newGrades) {
+      if (item.id === student.studentId) {
+        item.grade = student.grade;
+      }
+    }
+    return item;
+  });
+  return listOfStudents;
+}

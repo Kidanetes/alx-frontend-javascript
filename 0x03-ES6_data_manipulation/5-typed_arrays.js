ac2aea76656d66a99c9b9359bf3ee314a320a95b
kidanetes <kidanemaryamtesfaye@gmail.com>
2024-10-11 14:09:40 +0300

    data manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
+  const arr = new ArrayBuffer(length);
+  const val = new DataView(arr);
+
+  try {
+    val.setInt8(position, value);
+  } catch (e) {
+    throw Error('Position outside range');
   }
-  int8View[position] = value;
-  return buffer;
+  return val;
 }
